90b25796f3305f818153b9097c7e8ba2a52de926
Grace <gwadholm@gmail.com>
2023-10-19 12:15:47 -0400

    changed file structure, added css and js files

---
+++ js/global.js
@@ -0,0 +1,11 @@
+(function(){
+//digital clock function
+function displayTime(){
+  const clock = document.querySelector("#clock");
+  const now = new Date();
+  clock.textContent = now.toLocaleTimeString();
+}
+displayTime()
+setInterval(displayTime, 1000);
+
+})()
