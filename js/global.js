ac2d364cecb5e0d1222b8ae4ec8f241bef797d20
gwadholm <gwadholm@gmail.com>
2023-10-20 14:12:39 -0400

    many changes to interface, added img folder and svg images

--- js/global.js
+++ js/global.js
@@ -5,9 +5,23 @@
 // p2.textContent = "A beautiful butterfly"
 let body = document.querySelector("body");
 
-document.querySelector('h1').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
+document.querySelector('h1#myTag').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
 document.querySelector('h2').insertAdjacentHTML("beforebegin", `<img src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
 
+const topicButton = `
+<span class="topicButtonContainer">
+  <input id="topic1" type="radio" name="topic" value="topic" />
+  <label for="topic1" class="topicButton">
+  Nature
+  </label>  
+</span>`;          
+
+
+document.querySelector("fieldset").insertAdjacentHTML("afterbegin", `<div class="actionGroup">${topicButton}${topicButton}${topicButton}${topicButton}${topicButton}</div>`);
+
+
+
+//lightbox
 document.querySelector("h1").addEventListener('click', () => {
   
   body.insertAdjacentHTML("beforeend", `<div class="lightboxDiv"><img class="lightbox" src="https://design.gracetoread.com/lb/img/clipart/nature/monarch_01.png" /></div>`);
@@ -21,14 +35,9 @@ document.querySelector("h1").addEventListener('click', () => {
       e.target.remove();
       document.querySelector("div.lightboxDiv").remove();
     }
-  });
-
+  })
 });
-
-
-
-
-
+ 
 //digital clock function
 function displayTime(){
   const clock = document.querySelector("#clock");
@@ -39,3 +48,4 @@ displayTime()
 setInterval(displayTime, 1000);
 
 })()
+
