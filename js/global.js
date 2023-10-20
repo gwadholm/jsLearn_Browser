c54c77cf8a469f2773d50f84244a0577725942f6
Grace <gwadholm@gmail.com>
2023-10-20 09:46:28 -0400

    lots of changes and additions to code. Added a lightbox function and several page elements

--- js/global.js
+++ js/global.js
@@ -1,4 +1,34 @@
 (function(){
+// const body = document.querySelector("body");
+// const p2 = document.createElement("p");
+// body.appendChild(h2);
+// p2.textContent = "A beautiful butterfly"
+let body = document.querySelector("body");
+
+document.querySelector('h1').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
+document.querySelector('h2').insertAdjacentHTML("beforebegin", `<img src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
+
+document.querySelector("h1").addEventListener('click', () => {
+  
+  body.insertAdjacentHTML("beforeend", `<div class="lightboxDiv"><img class="lightbox" src="https://design.gracetoread.com/lb/img/clipart/nature/monarch_01.png" /></div>`);
+  
+
+  document.querySelector(".lightbox").addEventListener('click', (e) => {
+
+    if (e.target.getAttribute('class') === "lightboxDiv") {
+      e.target.remove();}
+    else if (e.target.getAttribute('class') === "lightbox") {
+      e.target.remove();
+      document.querySelector("div.lightboxDiv").remove();
+    }
+  });
+
+});
+
+
+
+
+
 //digital clock function
 function displayTime(){
   const clock = document.querySelector("#clock");
