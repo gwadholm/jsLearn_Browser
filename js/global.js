e58be9d47eb26db762a671e248e6d2042e1a939f
gwadholm <gwadholm@gmail.com>
2023-10-26 10:30:46 -0400

    refined the responsive design of the bubbles, and refined the search bar

--- js/global.js
+++ js/global.js
@@ -1,55 +1,46 @@
 (function(){
-// const body = document.querySelector("body");
-// const p2 = document.createElement("p");
-// body.appendChild(h2);
-// p2.textContent = "A beautiful butterfly"
-let body = document.querySelector("body");
 
-document.querySelector('h1#myTag').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
-document.querySelector('h2').insertAdjacentHTML("beforebegin", `<img class="normal" src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
+  let body = document.querySelector("body");
 
-// const topicButton = `
-// <div class="topicButtonContainer">
-//   <input id="topic1" type="radio" name="topic" value="topic" />
-//   <label for="topic1" class="topicButton">
-//   Nature
-//   </label>  
-// </div>`;          
+  document.querySelector('h1#myTag').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
 
+  document.querySelector('h2').insertAdjacentHTML("beforebegin", `<img class="thumb" src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
 
-// document.querySelector("fieldset").insertAdjacentHTML("afterbegin", `<div class="actionGroup">${topicButton}${topicButton}${topicButton}${topicButton}${topicButton}</div>`);
+  //lightbox
+  document.querySelector(".thumb").addEventListener('click', function(e) {
 
+    body.insertAdjacentHTML("beforeend", `<div class="lightboxDiv"><img class="lightbox" src="https://design.gracetoread.com/lb/img/clipart/nature/monarch_01.png" /></div>`);
 
-
-//lightbox
-document.querySelector(".normal").addEventListener('click', () => {
-  const screenWidth = `ScreenWidth: ${screen.width}` 
-  const screenHeight = `ScreenHeight: ${screen.height}` 
-
-  body.insertAdjacentHTML("beforeend", `<div class="lightboxDiv"><img class="lightbox" src="https://design.gracetoread.com/lb/img/clipart/nature/monarch_01.png" /></div>`);
-
-  console.log(screenWidth + " " + screenHeight);
-  
-
-  document.querySelector(".lightbox").addEventListener('click', (e) => {
-
-    if (e.target.getAttribute('class') === "lightboxDiv") {
-      e.target.remove();}
-    else if (e.target.getAttribute('class') === "lightbox") {
-      e.target.remove();
+    document.querySelector(".lightboxDiv").addEventListener('click', (e) => {
       document.querySelector("div.lightboxDiv").remove();
-    }
+      })
   })
-});
+
+})()
+
+
+
+
+
+
+
+
+
+
+/*
+const body = document.querySelector("body");
+const p2 = document.createElement("p");
+body.appendChild(h2);
+p2.textContent = "A beautiful butterfly"
+
+
 
 document.querySelector("#headNav").addEventListener('pointerleave', function() {
   console.log("missed me!");
 })
 
+file:///c%3A/Users/gwadh/Documents/Valencia/Fall2023/WebProgramming/jsLearn_Browser/jsLearn_Browser/index.html
 
-})()
-
-/*
 //digital clock function
 function displayTime(){
   const clock = document.querySelector("#clock");
@@ -60,5 +51,19 @@ displayTime()
 setInterval(displayTime, 1000);
 
 
-file:///c%3A/Users/gwadh/Documents/Valencia/Fall2023/WebProgramming/jsLearn_Browser/jsLearn_Browser/index.html
+
+const topicButton = `
+<div class="topicButtonContainer">
+  <input id="topic1" type="radio" name="topic" value="topic" />
+  <label for="topic1" class="topicButton">
+  Nature
+  </label>  
+</div>`;          
+
+
+document.querySelector("fieldset").insertAdjacentHTML("afterbegin", `<div class="actionGroup">${topicButton}${topicButton}${topicButton}${topicButton}${topicButton}</div>`);
+
+
+
 */
+
