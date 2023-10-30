61ec6470d9de7d700ccceec98f284c4b3c44098b
gwadholm <gwadholm@gmail.com>
2023-10-30 10:15:16 -0400

    new images, worked on form and adding nav search words

--- js/global.js
+++ js/global.js
@@ -2,9 +2,7 @@
 
   let body = document.querySelector("body");
 
-  document.querySelector('h1#myTag').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
-
-  document.querySelector('h2').insertAdjacentHTML("beforebegin", `<img class="thumb" src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
+  document.querySelector('h1#myTag').insertAdjacentHTML("beforeend", `<img class="thumb" src="https://design.gracetoread.com/img/clipart/nature/monarch_01.png" />`);
 
   //lightbox
   document.querySelector(".thumb").addEventListener('click', function(e) {
@@ -28,6 +26,9 @@
 
 
 /*
+
+document.querySelector('h1#myTag').insertAdjacentHTML("afterend" ,"<h2>My clipart library</h2>");
+
 const body = document.querySelector("body");
 const p2 = document.createElement("p");
 body.appendChild(h2);
